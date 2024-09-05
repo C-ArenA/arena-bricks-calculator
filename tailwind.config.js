346b7e36aa0fc9ea0c313207a308b48d1db5a99a
c-arena <carlostata.ca@gmail.com>
2024-09-05 18:30:26 -0400

    chore: Uninstall flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-import plugin from 'flowbite/plugin';
 export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', "./node_modules/flowbite/**/*.js"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {}
   },
-  plugins: [plugin]
+  plugins: []
 }
