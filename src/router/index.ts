7e46ef729899481a709226160f1038227f027df3
c-arena <carlostata.ca@gmail.com>
2024-09-05 18:28:05 -0400

    feature: Create route for the calculator

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,12 +10,19 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/calculator',
+      name: 'calculator',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/CalculatorView.vue'),
+      children: [
+        {
+          path: '',
+          name: 'walls-form',
+          component: () => import('@/views/WallFormView.vue')
+        }
+      ]
     }
   ]
 })
