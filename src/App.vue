4d243096f8e133b382acfe86d5a7bc53a8cd83b9
C-ArenA <carlostata.ca@gmail.com>
2024-09-06 04:20:53 -0400

    feature: update homepage

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import Menubar from 'primevue/menubar'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import type { MenuItem } from 'primevue/menuitem'
+
+const router = useRouter()
+const menuItems = ref<MenuItem[]>([
+  {
+    label: 'Home',
+    icon: 'pi pi-home',
+    command: () => router.push('/')
+  },
+  {
+    label: 'Calculadora',
+    icon: 'pi pi-calculator',
+    command: () => router.push('/calculator')
+  }
+])
 </script>
 
 <template>
   <header>
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <Menubar class="m-2" :model="menuItems" />
   </header>
-
-
-
-
-  <RouterView />
+  <main class="px-3">
+    <RouterView />
+  </main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,37 @@
 <script setup lang="ts">
+import Button from 'primevue/button'
+import RiArrowRightLine from '~icons/ri/arrow-right-line'
+import PhWallFill from '~icons/ph/wall-fill'
 </script>
 
 <template>
-  <main class="text-center my-40 max-w-6xl mx-auto">
+  <section class="text-center my-20 max-w-6xl mx-auto">
+    <PhWallFill class="m-auto text-6xl mb-5" />
     <h1
-      class="mb-10 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-      Cuántos Ladrillos <span
-        class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">ArenA</span> necesitarás para tu
-      muro?</h1>
-    <p class="mb-16 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-      Calcula facilmente la cantidad que requieres usando nuestra fácil calculadora de ladrillos. Muro a muro, ladrillo a ladrillo, fábrica de ladrillos ArenA siempre contigo
+      class="mb-10 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+    >
+      Cuántos Ladrillos
+      <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400"
+        >ArenA</span
+      >
+      necesitarás para tu muro?
+    </h1>
+    <p
+      class="mb-12 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"
+    >
+      Calcula facilmente la cantidad que requieres usando nuestra fácil calculadora de ladrillos.
+      Muro a muro, ladrillo a ladrillo, fábrica de ladrillos ArenA siempre contigo
     </p>
-    <RouterLink to="/calculator"
-      class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-      Inciar Calculadora
-      <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-        viewBox="0 0 14 10">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-          d="M1 5h12m0 0L9 1m4 4L9 9" />
-      </svg>
-    </RouterLink>
-    <p class="text-sm mt-16 text-gray-400">Si prefieres una atención personalizada puedes
-      contactarnos al 73745255 o al 73546240 vía Whatsapp.</p>
-  </main>
+    <Button as="router-link" label="Iniciar Calculadora" to="/calculator" iconPos="right">
+      <template #icon="slotProps">
+        <div :class="slotProps.class">
+          <RiArrowRightLine class="block" />
+        </div>
+      </template>
+    </Button>
+    <p class="text-sm mt-16 text-gray-400">
+      Si prefieres una atención personalizada puedes contactarnos al 73745255 o al 73546240 vía
+      Whatsapp.
+    </p>
+  </section>
 </template>
