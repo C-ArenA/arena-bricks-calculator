2aa43e25cdb1aa5831620ceb9d77645d01693e1f
C-ArenA <carlostata.ca@gmail.com>
2024-09-07 02:38:29 -0400

    feature: use primevue tailwind utility classes to improve styling

--- src/views/CalculatorView.vue
+++ src/views/CalculatorView.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import FluentMdl2FitWidth from '~icons/fluent-mdl2/fit-width'
 import PhWallFill from '~icons/ph/wall-fill'
 import FeInfo from '~icons/fe/info'
 import MaterialSymbolsWidth from '~icons/material-symbols/width'
@@ -7,6 +6,7 @@ import MaterialSymbolsHeight from '~icons/material-symbols/height';
 import Stepper from 'primevue/stepper'
 import StepItem from 'primevue/stepitem'
 import StepPanel from 'primevue/steppanel'
+import Panel from 'primevue/panel';
 import Step from 'primevue/step'
 import Button from 'primevue/button'
 import InputNumber from 'primevue/inputnumber'
@@ -15,98 +15,65 @@ import Select from 'primevue/select'
 import Card from 'primevue/card'
 import InputGroupAddon from 'primevue/inputgroupaddon'
 import { ref } from 'vue'
-import type {Unit} from '@/types/units'
+import type { Unit } from '@/types/units'
 
 const h = ref(0)
 const units = ref<Unit[]>([
-  { id: 1, name: 'm', relationToDefault: 1, type: 'length'},
-  { id: 2, name: 'cm', relationToDefault: 0.01, type: 'length'},
-  { id: 3, name: 'inch', relationToDefault: 0.0254, type: 'length'},
-  { id: 4, name: 'mm', relationToDefault: 0.001, type: 'length'}
+  { id: 1, name: 'm', relationToDefault: 1, type: 'length' },
+  { id: 2, name: 'cm', relationToDefault: 0.01, type: 'length' },
+  { id: 3, name: 'inch', relationToDefault: 0.0254, type: 'length' },
+  { id: 4, name: 'mm', relationToDefault: 0.001, type: 'length' }
 ])
 const selectedUnit = ref<Unit>(units.value[0])
 </script>
 
 <template>
-  <h2 class="text-4xl font-extrabold dark:text-white mb-4">Calculadora de Ladrillos ArenA</h2>
-
+  <h2 class="text-4xl font-extrabold text-color mb-4">Calculadora de Ladrillos ArenA</h2>
   <Stepper value="1">
     <StepItem value="1">
-      <Step>Define tus Muros</Step>
-      <StepPanel v-slot="{ activateCallback }">
-        <Card class="outline outline-1 outline-gray-600">
-          <template #title>
-            <div class="flex items-center gap-4 mb-5">
+      <Step>Definir Dimensiones del Muro</Step>
+      <StepPanel header="fdfd" v-slot="{ activateCallback }">
+        <Panel header="Muro" toggleable>
+          <template #header>
+            <div class="flex items-center gap-4">
               <PhWallFill class="text-2xl" />
-              <h4 class="text-2xl font-bold dark:text-white">Muro 1</h4>
+              <h4 class="text-2xl font-bold text-color">Muro 1</h4>
             </div>
           </template>
-          <template #content>
-            <label for="width" class="block mb-3 flex gap-2 items-center"
-              ><i class="inline">
-                <FeInfo class="inline" />
-              </i>
-              Ancho</label
-            >
-            <InputGroup class="mb-5">
-              <InputGroupAddon>
-                <MaterialSymbolsWidth class="text-2xl" />
-              </InputGroupAddon>
-              <InputNumber
-                v-model="h"
-                inputId="width"
-                :useGrouping="false"
-                :minFractionDigits="0"
-                :maxFractionDigits="5"
-                locale="es-ES"
-                style="width: 100%"
-              />
-              <Select
-                v-model="selectedUnit"
-                :options="units"
-                optionLabel="name"
-                placeholder="m"
-                class="grow-0 w-auto"
-                style="width: unset"
-              />
-            </InputGroup>
+          <label for="width" class="block mb-3 flex gap-2 items-center"><i class="inline">
+              <FeInfo class="inline" />
+            </i>
+            Ancho</label>
+          <InputGroup class="mb-5">
+            <InputGroupAddon>
+              <MaterialSymbolsWidth class="text-2xl" />
+            </InputGroupAddon>
+            <InputNumber v-model="h" inputId="width" :useGrouping="false" :minFractionDigits="0" :maxFractionDigits="5"
+              locale="es-ES" style="width: 100%" />
+            <Select v-model="selectedUnit" :options="units" optionLabel="name" placeholder="m" class="grow-0 w-auto"
+              style="width: unset" />
+          </InputGroup>
 
-            <label for="height" class="block mb-3 flex gap-2 items-center"
-              ><i class="inline">
-                <FeInfo class="inline" />
-              </i>
-              Alto</label
-            >
-            <InputGroup class="mb-5">
-              <InputGroupAddon>
-                <MaterialSymbolsHeight class="text-2xl" />
-              </InputGroupAddon>
-              <InputNumber
-                v-model="h"
-                inputId="height"
-                :useGrouping="false"
-                :minFractionDigits="0"
-                :maxFractionDigits="5"
-                locale="es-ES"
-                style="width: 100%"
-              />
-              <Select
-                v-model="selectedUnit"
-                :options="units"
-                optionLabel="name"
-                placeholder="m"
-                class="grow-0 w-auto"
-                style="width: unset"
-              />
-            </InputGroup>
-            <p>Area Calculada: 45 m2</p>
-          </template>
-        </Card>
+          <label for="height" class="block mb-3 flex gap-2 items-center"><i class="inline">
+              <FeInfo class="inline" />
+            </i>
+            Alto</label>
+          <InputGroup class="mb-5">
+            <InputGroupAddon>
+              <MaterialSymbolsHeight class="text-2xl" />
+            </InputGroupAddon>
+            <InputNumber v-model="h" inputId="height" :useGrouping="false" :minFractionDigits="0" :maxFractionDigits="5"
+              locale="es-ES" style="width: 100%" />
+            <Select v-model="selectedUnit" :options="units" optionLabel="name" placeholder="m" class="grow-0 w-auto"
+              style="width: unset" />
+          </InputGroup>
+          <p>Area Calculada: 45 m2</p>
+        </Panel>
         <div class="flex justify-center mt-6">
-          <Button icon="pi pi-plus" iconPos="bottom" rounded />
+          <Button icon="pi pi-plus" severity="secondary" iconPos="bottom" rounded />
         </div>
-        <div class="py-6">
-          <Button label="Next" severity="info" @click="activateCallback('2')" />
+        <div class="py-6 text-center">
+          <Button label="Next" @click="activateCallback('2')" />
         </div>
       </StepPanel>
     </StepItem>
@@ -130,12 +97,4 @@ const selectedUnit = ref<Unit>(units.value[0])
   </Stepper>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,20 +7,14 @@ import PhWallFill from '~icons/ph/wall-fill'
 <template>
   <section class="text-center my-20 max-w-6xl mx-auto">
     <PhWallFill class="m-auto text-6xl mb-5" />
-    <h1
-      class="mb-10 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
-    >
+    <h1 class="mb-10 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl">
       Cuántos Ladrillos
-      <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400"
-        >ArenA</span
-      >
+      <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">ArenA</span>
       necesitarás para tu muro?
     </h1>
-    <p
-      class="mb-12 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"
-    >
-      Calcula facilmente la cantidad que requieres usando nuestra fácil calculadora de ladrillos.
-      Muro a muro, ladrillo a ladrillo, fábrica de ladrillos ArenA siempre contigo
+    <p class="mb-12 text-lg font-normal lg:text-xl sm:px-16 xl:px-48 ">
+      Calcula facilmente la cantidad que requieras usando nuestra fácil calculadora. Ladrillo a ladrillo, muro a muro,
+      <span class="text-color-emphasis font-bold underline underline-offset-4">Cerámica ArenA</span> siempre contigo
     </p>
     <Button as="router-link" label="Iniciar Calculadora" to="/calculator" iconPos="right">
       <template #icon="slotProps">
@@ -29,9 +23,9 @@ import PhWallFill from '~icons/ph/wall-fill'
         </div>
       </template>
     </Button>
-    <p class="text-sm mt-16 text-gray-400">
-      Si prefieres una atención personalizada puedes contactarnos al 73745255 o al 73546240 vía
-      Whatsapp.
+    <p class="text-sm mt-16 text-muted-color">
+      Si prefieres una atención personalizada puedes contactarnos vía Whatsapp a los siguientes números: 73745255 -
+      73546240
     </p>
   </section>
 </template>
