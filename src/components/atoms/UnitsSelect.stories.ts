d03f5bb6fd5962136c0eb7f6a104043bc6e448cb
C-ArenA <carlostata.ca@gmail.com>
2024-09-08 19:13:25 -0400

    test: Create stories for UnitsSelect component

---
+++ src/components/atoms/UnitsSelect.stories.ts
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import UnitsSelect from "./UnitsSelect.vue";
+
+const meta: Meta<typeof UnitsSelect> = {
+    component: UnitsSelect,
+};
+export default meta;
+
+type Story = StoryObj<typeof UnitsSelect>;
+
+export const Length: Story = {
+    args: {
+        dimensionName: "length",
+    },
+};
+export const Area: Story = {
+    args: {
+        dimensionName: "area",
+    },
+};
+export const Volume: Story = {
+    args: {
+        dimensionName: "volume",
+    },
+};
+export const Mass: Story = {
+    args: {
+        dimensionName: "mass",
+    },
+};
+export const Time: Story = {
+    args: {
+        dimensionName: "time",
+    },
+};
