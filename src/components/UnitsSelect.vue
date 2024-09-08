7cad3f6d21dd2efff6218172e0a6841fc68fe1ef
C-ArenA <carlostata.ca@gmail.com>
2024-09-08 04:35:25 -0400

    feature: Create select component to work with units

---
+++ src/components/UnitsSelect.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Select from 'primevue/select';
+import { ref, watchEffect } from 'vue'
+import type { Unit, DimensionString } from '@/types/quantities'
+import { dimensions, units } from '@/data/units';
+
+interface Props {
+    dimensionName: DimensionString
+}
+const props = defineProps<Props>()
+
+const selectedUnit = defineModel<Unit | undefined>()
+const filteredUnits = ref<Unit[]>([])
+
+watchEffect(() => {
+    console.log('Miro props')
+    filteredUnits.value = units.filter(unit => unit.dimensionName === props.dimensionName)
+    selectedUnit.value = dimensions.find(d => d.name === props.dimensionName)?.baseUnit
+})
+</script>
+
+<template>
+    <Select v-model="selectedUnit" :options="filteredUnits" optionLabel="symbol" placeholder="ux"
+        class="grow-0 w-auto" style="width: unset" />
+</template>
