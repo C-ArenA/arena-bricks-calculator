76ea041816c38c73887f7cafc6c960dbfc153c6c
C-ArenA <carlostata.ca@gmail.com>
2024-09-09 12:17:47 -0400

    fix: Delete wallform view which doesnt exist anymore

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,13 +16,6 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/CalculatorView.vue'),
-      children: [
-        {
-          path: '',
-          name: 'walls-form',
-          component: () => import('@/views/WallFormView.vue')
-        }
-      ]
     }
   ]
 })
