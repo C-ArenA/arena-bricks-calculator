970217b247a497e58e1583613027dcc0a26e8523
c-arena <carlostata.ca@gmail.com>
2024-09-12 16:35:13 -0400

    feature: Create brick selector component

---
+++ src/components/organisms/BrickSelect.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import BrickSelect from './BrickSelect.vue'
+import { ref } from 'vue'
+import type { Brick } from '@/types/materials'
+
+const meta:Meta<typeof BrickSelect> = {
+  component: BrickSelect
+}
+export default meta
+type Story = StoryObj<typeof meta>
+
+const render = (args: any) => ({
+  components: { BrickSelect },
+  setup() {
+    const brick = ref<Brick>()
+    return { args: args, brick }
+  },
+  template: `<BrickSelect v-bind="args" v-model="brick"/>`,
+})
+
+export const Default: Story = {
+  render: render,
+  args: {},
+}
---
+++ src/components/organisms/BrickSelect.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import CardSelect from '../molecules/CardSelect.vue';
+import { bricks } from '@/data/materials'
+import { type Brick } from '@/types/materials';
+
+const selectedBrick = defineModel<Brick>()
+</script>
+
+<template>
+  <CardSelect v-model="selectedBrick" :options="bricks" titleKey="name" descriptionKey="description">
+    <template #subtitle="{option}">L: {{ option.length * 1000 }} x H: {{ option.height * 1000 }} x W: {{ option.width * 1000 }}</template>
+  </CardSelect>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
