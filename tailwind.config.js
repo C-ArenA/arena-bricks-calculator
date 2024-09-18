1504e696b991f10266a508503737b45a4e5b4cbf
C-ArenA_WSL <carlostata.ca@gmail.com>
2024-09-18 12:39:44 -0400

    chore: Update packages and configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import tailwindPrimeUI from 'tailwindcss-primeui'
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {}
