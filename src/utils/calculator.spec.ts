e7a3667b1a5d4ff9c4aa4b6cd35536241d0b4be3
C-ArenA_WSL <carlostata.ca@gmail.com>
2024-09-20 16:14:15 -0400

    chore: Disable tsconfig.vitest.json weird error

---
+++ src/utils/calculator.spec.ts
@@ -0,0 +1,6 @@
+// TEST made ONLY for the tsconfig.vitest.json warning get disabled
+import { expect, test } from 'vitest'
+
+test('should calculate total walls area', () => {
+  expect(1).toBe(1)
+})
