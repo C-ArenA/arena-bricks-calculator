fcc7457956d0f36a715e43b2335dc07859c4cd61
C-ArenA_WSL <carlostata.ca@gmail.com>
2024-09-20 03:40:43 -0400

    feature: Create sensible inputs for the mortar form. Only the most important ones

--- src/components/organisms/MortarForm.vue
+++ src/components/organisms/MortarForm.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+//https://wecivilengineers.wordpress.com/2018/03/28/how-to-calculate-quantity-of-cement-sand-water-in-mortar-of-14/
 import Panel from 'primevue/panel'
 import QuantityInput from '../molecules/QuantityInput.vue'
 import ToggleSwitch from 'primevue/toggleswitch'
+import RadioButton from 'primevue/radiobutton'
 import UilWall from '~icons/uil/wall'
 import type { Mortar } from '@/types/materials'
 import { units } from '@/data/units'
+import { cements, validCementSandRatios } from '@/data/materials'
 
 const mortar = defineModel<Mortar>({
   required: true,
@@ -25,6 +28,28 @@ const mortar = defineModel<Mortar>({
       :iconComponent="UilWall"
       dimensionName="length"
       :defaultUnit="units.find((u) => u.name === 'milímetro')" />
+    <h3 class="mb-2">Proporción cemento:arena</h3>
+    <div class="flex flex-wrap gap-4 mb-4">
+      <div v-for="(validCementSandRatio, index) in validCementSandRatios" :key="index" class="">
+        <RadioButton
+          v-model="mortar.cementSandRatio"
+          :value="validCementSandRatio"
+          :inputId="index.toString()"
+          name="cement-sand-ratio" />
+        <label :for="index.toString()" class="px-2">{{ validCementSandRatio[0] }} : {{ validCementSandRatio[1] }}</label>
+      </div>
+    </div>
+    <h3 class="mb-2">Tipo de cemento</h3>
+    <div class="flex flex-wrap gap-4 mb-4">
+      <div v-for="(cement, index) in cements" :key="index" class="">
+        <RadioButton
+          v-model="mortar.cement"
+          :value="cement"
+          :inputId="index.toString()"
+          name="cement" />
+        <label :for="index.toString()" class="px-2">Cemento {{ cement.name }}</label>
+      </div>
+    </div>
   </Panel>
 </template>
 
