3ba09e003eb163e8ba46b020625e713f0e2ac22f
C-ArenA_WSL <carlostata.ca@gmail.com>
2024-09-20 03:41:41 -0400

    feature: Make calculations for mortar materials

--- src/stores/calculator.ts
+++ src/stores/calculator.ts
@@ -15,6 +15,16 @@ export const useCalculatorStore = defineStore('calculator', () => {
   const totalBricksNeeded = computed(() =>
     calculateBricksNeeded(walls.value, brick.value, mortar.value),
   )
-
-  return { walls, mortar, brick, wallsArea, totalBricksNeeded }
+  const mortarPerBlockArea = computed(() => mortar.value.width * (brick.value.length + brick.value.height + mortar.value.width))
+  const brickArea = computed(() => brick.value.length * brick.value.height)
+  const mortarBrickRatio = computed(() => mortarPerBlockArea.value / (brickArea.value + mortarPerBlockArea.value))
+  const mortarVolume = computed(() => mortarBrickRatio.value * wallsArea.value * brick.value.width)
+  const mortarDryVolume = computed(() => mortarVolume.value * mortar.value.dryWetFactor)
+  const cementNeededVolume = computed(() => mortarDryVolume.value * mortar.value.cementSandRatio[0] / (mortar.value.cementSandRatio[0] + mortar.value.cementSandRatio[1]))
+  const cementDensity = computed(() => mortar.value.cement.bagWeight / mortar.value.cement.bagVolume)
+  const cementNeededWeight = computed(() => cementNeededVolume.value * cementDensity.value)
+  const cementNeededBags = computed(() => Math.ceil(cementNeededWeight.value / mortar.value.cement.bagWeight))
+  const sandNeededVolume = computed(() => cementNeededVolume.value * mortar.value.cementSandRatio[1])
+  const waterNeeded = computed(() => cementNeededWeight.value * mortar.value.waterCementWeightRatio)
+  return { walls, mortar, brick, wallsArea, totalBricksNeeded, cementNeededBags, sandNeededVolume, waterNeeded, mortarDryVolume, mortarVolume}
 })
